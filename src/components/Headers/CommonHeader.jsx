6d93a53cc134bec7ba76eb14a63fd04b3d40c00a
Uttam <uttamkhuntia1998@gmail.com>
2024-10-31 02:26:18 +0530

    uttam

---
+++ src/components/Headers/CommonHeader.jsx
@@ -0,0 +1,15 @@
+import { Card, CardBody, CardTitle, Container, Row, Col } from "reactstrap";
+
+const CommonHeader = () => {
+    return (
+        <>
+            <div className="header bg-gradient-info pb-6 pt-5 pt-md-8">
+                <Container >
+
+                </Container>
+            </div>
+        </>
+    );
+};
+
+export default CommonHeader;
